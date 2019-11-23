96d7685a57314221d3e7b3a231aa1c4829435d59
steven85048 <steven85048@gmail.com>
2019-11-23 11:16:09 -0700

    Clean up extra server file; finished server starting client

--- messaging_system/server/test_server.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('127.0.0.1', 5005))
-data, addr = sock.recvfrom(1024)
-print(data)
-print(addr)
-sock.sendto("reply from server".encode(), ('127.0.0.1', 5006))
