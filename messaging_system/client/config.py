1097c23c571fff510ec2406dde1c76024dab6bdb
steven85048 <steven85048@gmail.com>
2019-12-04 20:40:00 -0700

    Change client config to be on port 0.0.0.0 instead of loopback

--- messaging_system/client/config.py
+++ messaging_system/client/config.py
@@ -1,5 +1,5 @@
 client_config = dict(
-    SERVER_IP_ADDR = "127.0.0.1",
+    SERVER_IP_ADDR = "0.0.0.0",
     UDP_PORT = 0, # any open port
     BUFFER_MAX_SIZE = 1024,
     SOCKET_TIMEOUT = 1000.0
